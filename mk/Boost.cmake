07be3e863f30e459ccddd9f790699e256a1f9edc
dm <murzzz@gmail.com>
2016-11-07 22:27:16 +0200

    boost anc clang
    - find boost for clang based build also
    - do not install libboost from repos

--- mk/Boost.cmake
+++ mk/Boost.cmake
@@ -1,4 +1,4 @@
 
-if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+#if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
     find_package(Boost 1.60 COMPONENTS unit_test_framework program_options REQUIRED)
-endif()
+#endif()
