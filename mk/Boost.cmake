29e39c833533ee868ddeb7d62f17ac4c36728e8b
dm <dm@xps>
2017-10-10 22:59:15 +0300

    added sanitizers

--- mk/Boost.cmake
+++
@@ -1,4 +0,0 @@
-
-#if("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
-    find_package(Boost 1.60 COMPONENTS unit_test_framework system program_options REQUIRED)
-#endif()
--- mk/add_test.cmake
+++ mk/add_test.cmake
@@ -1,8 +1,12 @@
 enable_testing()
 
+find_package(Boost 1.60 COMPONENTS unit_test_framework system program_options REQUIRED)
+find_package(Sanitizers REQUIRED)
+
 macro(adventofcode_add_test DAY_NAME)
 
     add_executable(${DAY_NAME} test.cpp)
+    add_sanitizers(${DAY_NAME})
     target_include_directories(${DAY_NAME} PRIVATE ${Boost_INCLUDE_DIRS} ${CMAKE_SOURCE_DIR})
     target_link_libraries(${DAY_NAME} ${Boost_LIBRARIES})
     
